70070374193c8524ff8f06f30ac52c99a2c2646d
Amitrajeet Konch <amitrajeet@Amitrajeets-MacBook-Air.local>
2025-08-22 09:45:17 +0530

    inital sdk setup

---
+++ Loopr-SDK/src/payment/createIntent.ts
@@ -0,0 +1,30 @@
+
+import { PaymentIntentConfig } from './types';
+import { encodeURL } from '@solana/pay';
+import { PublicKey } from '@solana/web3.js';
+import { BigNumber } from 'bignumber.js';
+
+/**
+ * Converts a payment config to Solana Pay URI
+ */
+export function createPaymentIntent(config: PaymentIntentConfig): string {
+  const { recipient, amount, frequency, label, message, metadata } = config;
+
+  const recipientPK = new PublicKey(recipient);
+
+  // Prepare memo (this is how we embed extra metadata)
+  const memo = JSON.stringify({
+    frequency,
+    ...metadata
+  });
+
+  const url = encodeURL({
+    recipient: recipientPK,
+    amount: new BigNumber(amount),
+    label,
+    message,
+    memo
+  });
+
+  return url.toString(); // Returns: solana:<address>?amount=...&memo=...
+}
