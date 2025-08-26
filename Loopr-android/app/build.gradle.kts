1720354cb6e9fdbaa8118e9399cb3a8d0c019b32
Ambarish Manna <amanna1041@gmail.com>
2025-08-26 19:11:32 +0530

    Upcoming Payment Section

--- Loopr-android/app/build.gradle.kts
+++ Loopr-android/app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.kotlin.compose)
     id ("com.google.devtools.ksp")
     id ("com.google.dagger.hilt.android")
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.1.0"
 }
 
 android {
@@ -83,4 +84,7 @@ dependencies {
     implementation("androidx.security:security-crypto:1.1.0-alpha06")
 
     implementation("androidx.compose.material:material-icons-extended:1.7.8")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
+
 }
